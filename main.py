ef9bf1bbe928328740f76f115b2f02818f9dd2a8
Sam Zagrobelny <strangesast@gmail.com>
2020-10-17 14:30:20 -0400

    partial

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 """
 monitor machine table, monitor processes polling machines
 """
+import asyncpg
 import asyncio
+import signal
 from aiohttp import web
+
+async def init_db(app):
+    async with app['db'].acquire() as con:
+        await con.execute('''
+            drop table if exists machine_monitor;
+            create table machine_monitor(
+              id           text,
+              machine_ip   varchar(40),
+              machine_port integer
+            );
+            insert into machine_monitor(id,machine_ip,machine_port) values ('test','localhost',8193);
+        ''')
+
 
 
 async def handle(request):
@@ -11,32 +26,73 @@ async def handle(request):
     return web.Response(text=text)
 
 
-async def looper(app: web.Application):
+tasks = {}
+
+
+async def create_proc(id, machine_ip, machine_port):
     try:
         while True:
-            print('loop')
-            await asyncio.sleep(1)
+            proc = await asyncio.create_subprocess_shell(f'while true; do echo "{id=} {machine_ip=} {machine_port=}"; sleep 1; done', stdout=asyncio.subprocess.PIPE)
+            async for line in proc.stdout:
+                print('line', line)
+            await asyncio.sleep(5)
     except asyncio.CancelledError:
-        print('cancelled')
-    finally:
-        pass
+        proc.kill()
+        await proc.wait()
+
+
+async def init_tasks(app: web.Application):
+    async with app['db'].acquire() as con:
+        rows = await con.fetch('''select id,machine_ip,machine_port from machine_monitor''')
+
+        for row in rows:
+            tasks[row['id']] = asyncio.create_task(create_proc(*tuple(row)))
 
 
 async def start_background_tasks(app):
-    app['loop'] = asyncio.create_task(looper(app));
+    app['db'] = await asyncpg.create_pool(user='postgres', password='password', database='testing')
+    await init_db(app)
+    await init_tasks(app)
 
 
 async def cleanup_background_tasks(app):
+    tasks = tasks.values()
+    for task in tasks:
+        task.cancel()
+    await asyncio.gather(*tasks)
+    await app['db'].close();
     app['loop'].cancel()
     await app['loop']
 
 
-app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+async def init():
+    app = web.Application()
 
-app.on_startup.append(start_background_tasks)
-app.on_cleanup.append(cleanup_background_tasks)
+    #app.router.add_get('/', handler)
+
+    app.add_routes([web.get('/', handle),
+                    web.get('/{name}', handle)])
+    
+    app.on_startup.append(start_background_tasks)
+    app.on_cleanup.append(cleanup_background_tasks)
+
+    #app.on_startup.append(on_startup)
+    #app.on_cleanup.append(on_cleanup)
+    #runner = web.AppRunner(app)
+    #try:
+    #    await runner.setup()
+    #    site = web.TCPSite(runner, 'localhost', 8080)
+    #    await site.start()
+    #    await asyncio.sleep(1000)
+
+    #except asyncio.CancelledError:
+    #    await runner.cleanup()
+    return app
+
 
 if __name__ == '__main__':
-    web.run_app(app)
+    #asyncio.run(init())
+    #signal.signal(signal.SIGINT, lambda: task.cancel())
+    #asyncio.run(task)
+
+    web.run_app(init())
